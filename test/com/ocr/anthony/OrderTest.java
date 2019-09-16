2d116d57f38e4c33cd5ea32f85244072fdb51718
TRUBERT <jean.trubert@cgi.com>
2019-09-16 14:23:37 +0200

    1st Commit

---
+++ test/com/ocr/anthony/OrderTest.java
@@ -0,0 +1,54 @@
+package com.ocr.anthony;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class OrderTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    Order order = new Order();
+
+    @Test
+    public void Given_Chicken_When_DisplayMenuSelected_Then_DisplayChickenSentence() {
+        order.displaySelectedMenu(1);
+        assertEquals("Vous avez choisi comme menu : poulet\n", outContent.toString().replace("\r\n", "\n"));
+    }
+    @Test
+    public void Given_Beef_When_DisplayMenuSelected_Then_DisplayBeefSentence() {
+        order.displaySelectedMenu(2);
+        assertEquals("Vous avez choisi comme menu : boeuf\n", outContent.toString().replace("\r\n", "\n"));
+    }
+    @Test
+    public void Given_Vegetarian_When_DisplayMenuSelected_Then_DisplayVegetarianSentence() {
+        order.displaySelectedMenu(3);
+        assertEquals("Vous avez choisi comme menu : végétarien\n", outContent.toString().replace("\r\n", "\n"));
+    }
+    @Test
+    public void Given_TooBigValue_When_DisplayMenuSelected_Then_DisplayErrorSentence() {
+        order.displaySelectedMenu(15);
+        assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
+    }
+    @Test
+    public void Given_NegativeValue_When_DisplayMenuSelected_Then_DisplayErrorSentence() {
+        order.displaySelectedMenu(-6);
+        assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
+    }
+}
